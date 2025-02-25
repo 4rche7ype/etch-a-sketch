2aec39efc6825076b663f634a98fe1be33f1a245
Raymond <raydhz@pm.me>
2025-02-25 15:15:41 -0800

    Add the even to paint tile hovered over
    achieved by adding a class to modify background color

--- sketch.js
+++ sketch.js
@@ -6,9 +6,23 @@ const sketchPad = document.querySelector("#container");
 
 do {
     let gridTile = document.createElement("div");
+    gridTile.setAttribute("id", "tile" + counter.toString());
     gridTile.classList.add("tiles");
     sketchPad.appendChild(gridTile);
     counter++;
 } while (counter < sketchSize);
+
+const tileList = document.querySelectorAll(".tiles");
+
+sketchPad.addEventListener("mousedown", () => {
+    
+    tileList.forEach((item) => {
+        item.addEventListener("mouseover", () => {
+            item.classList.add("paintedTile");
+        });
+    });
+    
+});
+
 
 
